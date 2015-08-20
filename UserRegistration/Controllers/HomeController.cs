d1373c45aa21110c52f4d76dc0b2eddcf54de1f1
Josué Molina <molina.wilfredo.j@gmail.com>
2015-08-19 23:32:30 -0500

    First Commit

---
+++ UserRegistration/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using UserRegistration.Models;
+
+namespace UserRegistration.Controllers
+{
+    public class HomeController : AsyncController
+    {
+        private UserDbContext db = new UserDbContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ViewUsers()
+        {
+            return View("ViewUsers", db.Users.ToList());
+        }
+        public ActionResult CreateUserCompleted()
+        {
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public void CreateUserAsync(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                AsyncManager.OutstandingOperations.Increment();
+                db.Users.Add(user);
+                db.SaveChanges();
+                AsyncManager.OutstandingOperations.Decrement();
+            }
+        }
+    }
+}
---
+++ UserRegistration/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace UserRegistration.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "First Name is required.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required.")]
+        public string LastName { get; set; }
+    }
+
+    public class UserDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
